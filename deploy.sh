fedf60953bb7192888c2bb0e5aabdb1efed41711
AbdujalolAndy <mrarnoldunlimited@gmail.com>
2025-01-05 13:26:28 +0900

    feat: Deploying using deploy.sh & pm2

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+#PRODUCTION
+# git reset --hard
+# git checkout master 
+# git pull origin master
+
+npm i yarn -g
+yarn install
+yarn add global serve
+yarn run build
+
+pm2 start "yarn run start:prod" --name=ChatHub-React
